13a713082ba3ef19a879a354518235a94c865342
m.sam <64077684+m-sam-mohamadi@users.noreply.github.com>
2021-01-20 11:45:14 +0330

    added files

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '1534411815:AAGDRU9GAW8w0tNkyNkFbUTzXszP_7MxY0M';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+ 
+bot.onText("/start", (msg, match) => { 
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
